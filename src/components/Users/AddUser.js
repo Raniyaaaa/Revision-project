50a17ff03ab1294b2fdf081a5f8bc8cdfb1e312b
Raniyaaaa <raniya182002@gmail.com>
2024-10-20 16:32:22 +0530

    onChange eventlisteners added to name and age field , logging the inputs to console and clearing the input fields

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,32 @@
 
-import React from "react";
+import React,{useState} from "react";
 import "./AddUser.css"
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 const AddUser = () => {
+const [nameInput,setNameInput]=useState("");
+const [ageInput,setAgeInput]=useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(nameInput,ageInput);
+    setNameInput("");
+    setAgeInput("");
   };
+  const nameInputHandler=(event)=>{
+    setNameInput(event.target.value);
+  }
+  const ageInputHandler=(event)=>{
+    setAgeInput(event.target.value);
+  }
 
   return (
     <Card  className="input">
     <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
+      <input id="username" type="text" value={nameInput} onChange={nameInputHandler} />
       <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
+      <input id="age" type="number" value={ageInput} onChange={ageInputHandler}/>
       <Button type="submit">Add User</Button>
     </form>
     </Card>  
