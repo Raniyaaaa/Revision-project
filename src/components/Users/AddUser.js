e87a6bdcc64f064b1a29f6c4604b4259098dada7
Raniyaaaa <raniya182002@gmail.com>
2024-10-20 14:20:45 +0530

    creating Card components and applying styles to Card and AddUser

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,24 @@
+
 import React from "react";
-const AddUser=()=>{
-  const addUserHandler=(event)=>{
+import "./AddUser.css"
+import Card from "../UI/Card";
+
+const AddUser = () => {
+  const addUserHandler = (event) => {
     event.preventDefault();
-  }
-  return(
+  };
+
+  return (
+    <Card  className="input">
     <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
-      <input id="username"/>
+      <input id="username" type="text" />
       <label htmlFor="age">Age</label>
-      <input id="age"/>
+      <input id="age" type="number" />
       <button type="submit">Add User</button>
     </form>
-  )
-}
+    </Card>  
+  );
+};
+
 export default AddUser;
